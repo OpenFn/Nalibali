95b83a4e8c703a1f7aab45a55e0125836f2a2ef3
aleksa-krolls <aleksagk@gmail.com>
2020-01-18 16:08:08 +0200

    new jobs for E1M activities

---
+++ add-course.js
@@ -0,0 +1,16 @@
+upsert("Every1Mobile_Activity__c", "Activity_ID__c", fields(
+  field("Activity_ID__c", (state) => {
+    var user = state.data.payload.user_id;
+    var time = state.data.unix_timestamp;
+    var uid = user + time;
+    return uid;
+  }),
+  field("RecordTypeID", "0120N0000001XCxQAM"),
+  relationship("Contact__c", "Every1Mobile_ID__c", dataValue("payload.user_id")),
+  field("Type__c", dataValue("type")),
+  field("DateTime__c", (state) => {
+    return new Date(state.data.unix_timestamp * 1000);
+  }),
+  field("Course_Link__c", dataValue("payload.course_link")),
+  field("Course_Name__c", dataValue("payload.course_name"))
+));
---
+++ add-user-profile.js
@@ -0,0 +1,46 @@
+upsert("Contact", "Every1Mobile_ID__c", fields(
+  field("Every1Mobile_ID__c", dataValue("payload.profile.user_id")),
+  field("RecordTypeID", "012b00000009bWTAAY"),
+  field("Intake_Programme__c", "Funda Sonke"),
+  field("Description", dataValue("payload.profile.bio")),
+  field("MailingCity", dataValue("payload.profile.city")),
+  field("MailingCountry", "South Africa"),
+  field("Description", dataValue("payload.profile.bio")),
+  field("Birthdate", (state) => {
+    return new Date(state.data.payload.profile.dob * 1000); // check transformation ?
+  }),
+  field("Email", dataValue("payload.profile.email")),
+  field("FirstName", dataValue("payload.profile.first_name")),
+  field("LastName", dataValue("payload.profile.last_name")),
+  field("Gender__c", (state) => {
+    var gender = state.data.payload.gender;
+    if (gender==="f"){
+      return "Female";
+    } else if (gender==="m"){
+      return "Male";
+    } else {
+      return null;
+    }
+  }),
+  field("Is_Nal_ibali_staff_member__c", dataValue("payload.profile.nalibali_employee")),
+  field("Nal_ibali_Role__c", dataValue("payload.profile.nalibali_role")),
+  field("Phone", dataValue("payload.profile.phone")),
+  field("Province__c", dataValue("payload.profile.province")),
+  field("Date_of_E1M_Signup__c",(state) => {
+    const dateArray = state.data.payload.profile.ts.split(' ');
+    return dateArray[0];
+  })
+)),
+upsert("Every1Mobile_Activity__c", "Activity_ID__c", fields(
+  field("Activity_ID__c", (state) => {
+    var user = state.data.payload.profile.user_id;
+    var time = state.data.unix_timestamp;
+    var uid = user + time;
+    return uid;
+  }),
+  relationship("Contact__c", "Every1Mobile_ID__c", dataValue("payload.profile.user_id")),
+  field("DateTime__c", (state) => {
+    return new Date(state.data.unix_timestamp * 1000);
+  }),
+  field("Type__c", dataValue("type"))
+));
