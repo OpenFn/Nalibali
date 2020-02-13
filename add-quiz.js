30e767417cbe8e80a74157dd20e8c50f6655a978
Taylor Downs <downs.taylor@gmail.com>
2020-02-13 11:15:51 +0000

    refactor quiz and survey jobs

--- add-quiz.js
+++ add-quiz.js
@@ -1,47 +1,56 @@
-upsert("Every1Mobile_Activity__c", "Unique_ID__c", fields(
-  field("Unique_ID__c", (state) => {
-    var user = state.data.payload.quiz.user_id;
-    var time = state.data.unix_timestamp;
-    var uid = `${user}${time}`;
-    return uid;
-  }),
-  field("RecordTypeID", "0120N0000001XCsQAM"),
-  relationship("Contact__r", "Every1Mobile_ID__c", dataValue("payload.quiz.user_id")),
-  field("Type__c", dataValue("type")),
-  field("DateTime__c", (state) => {
-    return new Date(state.data.unix_timestamp * 1000);
-  }),
-  field("Quiz_ID__c", dataValue("payload.quiz.id"))
-)),
-//for each question in quiz, create 1 child Every1Mobile_Activity__c w/ self-lookup to parent above
-//path to question array: $.payload.quiz.questions[*]
-each(
-  merge(
-    dataPath("$.payload.quiz.questions[*]"),
-    fields(
-      field("userId", dataValue("payload.quiz.user_id")),
-      field("timestamp", dataValue("unix_timestamp"))
-    )
-  ),
-  upsert("Every1Mobile_Activity__c", "Unique_ID__c", fields(
-    field("Unique_ID__c", (state) => {
-      var user = state.data.userId;
-      var time = state.data.timestamp;
-      var question = state.data.id;
-      var uid = `${user}${time}${question}`;
-      return uid;
+alterState(state => {
+  var user = state.data.payload.quiz.user_id;
+  var time = state.data.unix_timestamp;
+  state.data._quizIdentifier = `${user}${time}`;
+  return state;
+});
+
+upsert(
+  'Every1Mobile_Activity__c',
+  'Unique_ID__c',
+  fields(
+    field('Unique_ID__c', dataValue('_quizIdentifier')),
+    field('RecordTypeID', '0120N0000001XCsQAM'),
+    relationship(
+      'Contact__r',
+      'Every1Mobile_ID__c',
+      dataValue('payload.quiz.user_id')
+    ),
+    field('Type__c', dataValue('type')),
+    field('DateTime__c', state => {
+      return new Date(state.data.unix_timestamp * 1000);
     }),
-    relationship("Contact__r", "Every1Mobile_ID__c", dataValue('userId')),
-    field("Survey_Question_Type__c", dataValue("type")),
-    field("Survey_Questions_ID__c", dataValue("id")),
-    field("Survey_Questions_Question__c", dataValue("question")),
-    field("Survey_Answer_ID__c", dataValue("answer.id")),
-    field("Survey_Questions_Answer__c", dataValue("answer.answer")),
-    relationship("Every1Mobile_Activity__r", "Unique_ID__c", (state) => {      
-      var user = state.data.userId;
-      var time = state.data.timestamp;
-      var uid = `${user}${time}`;
-      return uid;
-    })
-  ))
-);
+    field('Quiz_ID__c', dataValue('payload.quiz.id'))
+  )
+),
+  //for each question in quiz, create 1 child Every1Mobile_Activity__c w/ self-lookup to parent above
+  //path to question array: $.payload.quiz.questions[*]
+  each(
+    merge(
+      dataPath('$.payload.quiz.questions[*]'),
+      fields(
+        field('userId', dataValue('payload.quiz.user_id')),
+        field('quizId', dataValue('_quizIdentifier'))
+      )
+    ),
+    upsert(
+      'Every1Mobile_Activity__c',
+      'Unique_ID__c',
+      fields(
+        field('Unique_ID__c', state => {
+          return `${state.data.surveyId}${state.data.id}`;
+        }),
+        relationship('Contact__r', 'Every1Mobile_ID__c', dataValue('userId')),
+        field('Survey_Question_Type__c', dataValue('type')),
+        field('Survey_Questions_ID__c', dataValue('id')),
+        field('Survey_Questions_Question__c', dataValue('question')),
+        field('Survey_Answer_ID__c', dataValue('answer.id')),
+        field('Survey_Questions_Answer__c', dataValue('answer.answer')),
+        relationship(
+          'Every1Mobile_Activity__r',
+          'Unique_ID__c',
+          dataValue('quizId')
+        )
+      )
+    )
+  );
--- add-survey.js
+++ add-survey.js
@@ -1,47 +1,57 @@
-upsert("Every1Mobile_Activity__c", "Unique_ID__c", fields(
-    field("Unique_ID__c", (state) => {
-      var user = state.data.payload.survey.user_id;
-      var time = state.data.unix_timestamp;
-      var uid = `${user}${time}`;
-      return uid;
-    }),
-    field("RecordTypeID", "0120N0000001XCnQAM"),
-    relationship("Contact__r", "Every1Mobile_ID__c", dataValue("payload.survey.user_id")),
-    field("Type__c", dataValue("type")),
-    field("DateTime__c", (state) => {
+alterState(state => {
+  var user = state.data.payload.survey.user_id;
+  var time = state.data.unix_timestamp;
+  state.data._surveyIdentifier = `${user}${time}`;
+  return state;
+});
+
+upsert(
+  'Every1Mobile_Activity__c',
+  'Unique_ID__c',
+  fields(
+    field('Unique_ID__c', dataValue('_surveyIdentifier')),
+    field('RecordTypeID', '0120N0000001XCnQAM'),
+    relationship(
+      'Contact__r',
+      'Every1Mobile_ID__c',
+      dataValue('payload.survey.user_id')
+    ),
+    field('Type__c', dataValue('type')),
+    field('DateTime__c', state => {
       return new Date(state.data.unix_timestamp * 1000);
     }),
-    field("Survey_ID__c", dataValue("payload.survey.id"))
-  )),
-  //for each question in survey, create 1 child Every1Mobile_Activity__c w/ self-lookup to parent above
-  //path to question array: $.payload.survey.questions[*]
-  each(
-    merge(
-      dataPath("$.payload.survey.questions[*]"),
-      fields(
-        field("userId", dataValue("payload.survey.user_id")),
-        field("timestamp", dataValue("unix_timestamp"))
-      )
-    ),
-    upsert("Every1Mobile_Activity__c", "Unique_ID__c", fields(
-      field("Unique_ID__c", (state) => {
-        var user = state.data.userId;
-        var time = state.data.timestamp;
-        var question = state.data.id;
-        var uid = `${user}${time}${question}`;
-        return uid;
+    field('Survey_ID__c', dataValue('payload.survey.id'))
+  )
+);
+
+//for each question in survey, create 1 child Every1Mobile_Activity__c w/ self-lookup to parent above
+//path to question array: $.payload.survey.questions[*]
+each(
+  merge(
+    dataPath('$.payload.survey.questions[*]'),
+    fields(
+      field('userId', dataValue('payload.survey.user_id')),
+      field('surveyId', dataValue('_surveyIdentifier'))
+    )
+  ),
+  upsert(
+    'Every1Mobile_Activity__c',
+    'Unique_ID__c',
+    fields(
+      field('Unique_ID__c', state => {
+        return `${state.data.surveyId}${state.data.id}`;
       }),
-      relationship("Contact__r", "Every1Mobile_ID__c", dataValue("userId")),
-      field("Survey_Question_Type__c", dataValue("type")),
-      field("Survey_Questions_ID__c", dataValue("id")),
-      field("Survey_Questions_Question__c", dataValue("question")),
-      field("Survey_Answer_ID__c", dataValue("answer.id")),
-      field("Survey_Questions_Answer__c", dataValue("answer.answer")),
-      relationship("Every1Mobile_Activity__r", "Unique_ID__c", (state) => {
-        var user = state.data.userId;
-        var time = state.data.timestamp;
-        var uid = `${user}${time}`;
-        return uid;
-      })
-    ))
-  );
+      relationship('Contact__r', 'Every1Mobile_ID__c', dataValue('userId')),
+      field('Survey_Question_Type__c', dataValue('type')),
+      field('Survey_Questions_ID__c', dataValue('id')),
+      field('Survey_Questions_Question__c', dataValue('question')),
+      field('Survey_Answer_ID__c', dataValue('answer.id')),
+      field('Survey_Questions_Answer__c', dataValue('answer.answer')),
+      relationship(
+        'Every1Mobile_Activity__r',
+        'Unique_ID__c',
+        dataValue('surveyId')
+      )
+    )
+  )
+);
